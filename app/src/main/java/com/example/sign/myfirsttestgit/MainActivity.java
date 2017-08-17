db90333e12ede567cd0b02dcaeae354cfafeeb70
surachet teerapoj <bannsigndroid@users.noreply.github.com>
2017-08-17 08:11:21 +0700

    myfirst commit

---
+++ app/src/main/java/com/example/sign/myfirsttestgit/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.sign.myfirsttestgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // code something...
+
+        Button firstButton = (Button) findViewById(R.id.firstbutton);
+
+
+
+
+    }
+}
