8aaf5ffc035774f67a098fb9569f691044103853
surachet teerapoj <bannsigndroid@users.noreply.github.com>
2017-08-17 08:46:27 +0700

    แก้ เพิ่มButton

--- app/src/main/java/com/example/sign/myfirsttestgit/MainActivity.java
+++ app/src/main/java/com/example/sign/myfirsttestgit/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sign.myfirsttestgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,12 @@ public class MainActivity extends AppCompatActivity {
         // code something...
 
         Button firstButton = (Button) findViewById(R.id.firstbutton);
+        firstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
 
 
